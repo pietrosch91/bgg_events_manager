4f247c2355cb97fbaf5807de37fae49ba880c39a
Pietro Ottanelli <pietro.ottanelli@pi.infn.it>
2025-11-25 22:37:39 +0100

    fastcommit

--- projects/bgg-sql-mngr/src/lib/dataclasses/HTTP_utils.ts
+++ projects/bgg-sql-mngr/src/lib/dataclasses/HTTP_utils.ts
@@ -22,7 +22,7 @@ export class httpUtils{
     return this.postData('http://192.168.1.7:7777/select', { query: query });
   }
 
-  async insert(query: string): Promise<string>{
+  async insert(query: string): Promise<{sdata:string}>{
     return this.postData('http://192.168.1.7:7777/insert', { query: query });
   }
 
